1ad848dd7fca3f650ee7009d0144ee4cb50373b6
Ed Brannin <edbrannin@gmail.com>
2024-04-04 20:02:19 -0400

    Add heading & link to the puzzle book

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,15 @@ import { ConstraintsContext } from './useConstraints';
 import InputPalette from './InputPalette';
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   const [constraints, setConstraints] = useState<Constraints>([99, 99, 99, 99, 99, 99, 99, 99]);
   const [inputs, setInputs] = useState<Inputs>([0, 0, 0, 0, 0, 0, 0, 0, 0])
 
   return (
     <>
+      <h1>Circuit Board Square</h1>
+      <p>
+        For use with <a href="https://alexandershen.itch.io/circuit-board-square">Cuircuit Board Square</a> puzzles by <a href="https://alexandershen.itch.io/">Alexander Shen</a>
+      </p>
       <InputsContext.Provider value={{ inputs, setInputs }}>
         <ConstraintsContext.Provider value={{ constraints, setConstraints }}>
           <Board
